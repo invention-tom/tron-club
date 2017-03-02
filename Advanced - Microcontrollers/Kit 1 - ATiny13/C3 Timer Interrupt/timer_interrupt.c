fc21835f2b3c73a30c1cc2e9cc6e9df96da6347b
Tom Smith <tom@appliedinvention.com>
2017-03-01 21:27:40 -0800

    Two questions answered.
    1 - Use an interupt to flash an LED
    2 - Change the prescalers to 64

--- Advanced - Microcontrollers/Kit 1 - ATiny13/C3 Timer Interrupt/timer_interrupt.c
+++ Advanced - Microcontrollers/Kit 1 - ATiny13/C3 Timer Interrupt/timer_interrupt.c
@@ -9,19 +9,19 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
-#include <avr/interrupt.h> 	//Allows the use of interrupts
+#include <avr/interrupt.h> //Allows the use of interrupts
 
 ISR(TIM0_OVF_vect) {
-
-  PORTB ^= (1<<PB4);   //toggle LED ON/OFF
+  PORTB ^= (1<<PB4); //toggle LED ON/OFF
 }
 
 int main(void) {
-  cli();	//Clears the global interrupt flag
+  cli(); //Clears the global interrupt flag
   DDRB = (1<<DDB4);
   PORTB = 0;
-  TCCR0B |= (1<<CS02) | (1<<CS00) ;    //prescale timer by a factor of 1024
-  TIMSK0 |= (1<<TOIE0);    //enable timer overflow interrupt
-  sei();    //Sets global interrupt bit; Enables interrupts
-  while(1);	//loop forever, and let the interrupts do the work.
+  TCCR0B |= (1<<CS02) | (1<<CS00); //prescale timer by a factor of 1024
+  //TCCR0B |= (1<<CS01) | (1<<CS00); //prescale timer by a factor of 64
+  TIMSK0 |= (1<<TOIE0); //enable timer overflow interrupt
+  sei(); //Sets global interrupt bit; Enables interrupts
+  while(1); //loop forever, and let the interrupts do the work.
 }
