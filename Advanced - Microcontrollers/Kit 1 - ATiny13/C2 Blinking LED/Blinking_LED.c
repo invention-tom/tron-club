219d3671dcdecc7c0798515e23ba39bfb85db3b0
Tom Smith <tom@appliedinvention.com>
2017-03-01 21:19:18 -0800

    Four questions answered.
    1 - Blink LED on PB3
    2 - Change blink speed
    3 - Blink both LEDs at same time
    4 - Alternate blinking LEDs

--- Advanced - Microcontrollers/Kit 1 - ATiny13/C2 Blinking LED/Blinking_LED.c
+++ Advanced - Microcontrollers/Kit 1 - ATiny13/C2 Blinking LED/Blinking_LED.c
@@ -5,16 +5,22 @@
  */
 
 #include <avr/io.h>
-#include <util/delay.h> 						// Allows the use of delays
+#include <util/delay.h> // Allows the use of delays
 
 int main(void) {
-	DDRB = (1<<DDB4) | (1<<DDB3);; 		// Set PB3 and PB4 as output
-
-	for (;;){													//loop forever
-		PORTB = (1<<PB4) | (1<<PB3) ; 	//Set output of PB3 and PB4 as HIGH.
-		_delay_ms(500);
-		PORTB = (1<<PB4) ; 							//Set only PB4 as high, PB3 is low
-		_delay_ms(500);
-		}
+	DDRB = (1<<DDB4) | (1<<DDB3); // Set PB3 and PB4 as output
+	
+	for (;;) { //loop forever
+		PORTB = (1<<PB4) | (0<<PB3); //Set output of PB3 and PB4 as HIGH.
+		//_delay_ms(500); // Slower blinking
+		_delay_ms(100); // Faster blinking
+		
+		PORTB = (0<<PB4) | (1<<PB3); //Set only PB4 as high, PB3 is low
+		//PORTB = (1<<PB4); //Set only PB4 as high, PB3 is low
+		
+		//_delay_ms(500); // Slower blinking
+		_delay_ms(100); // Faster blinking
+	}
+	
 	return 0;
 }
