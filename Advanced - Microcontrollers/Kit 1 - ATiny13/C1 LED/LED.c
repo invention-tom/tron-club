3f29d359eb8fb78546820aed218be5c085e6f836
Tom Smith <tom@appliedinvention.com>
2017-03-01 21:09:40 -0800

    Three questions answered.
    1 - Turn on LED on PB4
    2 - Turn off LED on PB4
    3 - Turn on LED on PB3

--- Advanced - Microcontrollers/Kit 1 - ATiny13/C1 LED/LED.c
+++ Advanced - Microcontrollers/Kit 1 - ATiny13/C1 LED/LED.c
@@ -7,7 +7,19 @@
 #include <avr/io.h>
 
 int main(void) {
-	DDRB = (1<<DDB4);; // Set PB4 as output
-	PORTB = (1<<PB4); // Set output of PB4 as HIGH. Turns on LED. 
+	/* Turn on the LED connected to PB4
+	DDRB = (1<<DDB4); // Set PB4 as output.
+	PORTB = (1<<PB4); // Set output of PB4 as HIGH. Turns on LED.
+	*/
+	
+	/* Turn off the LED connected to PB4
+	DDRB = (1<<DDB4); // Set PB4 as output.
+	PORTB = (0<<PB4); // Set output of PB4 as LOW. Turns off LED.
+	*/
+	
+	// Turn on the LED connected to PB3
+	DDRB = (1<<DDB3); // Set PB3 as output.
+	PORTB = (1<<PB3); // Set output of PB3 as HIGH. Turns on LED.
+	
 	return 0;
 }
