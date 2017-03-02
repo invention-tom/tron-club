8e01b7830adb626569da754eba6405c8131e10c6
Tom Smith <tom@appliedinvention.com>
2017-03-01 21:43:21 -0800

    Two questions answered.
    1 - Flash LED on falling edge of button press
    2 - Flash LED on both falling / rising edge of button press

--- Advanced - Microcontrollers/Kit 1 - ATiny13/C4 Button Interrupt/button_interrupt.c
+++ Advanced - Microcontrollers/Kit 1 - ATiny13/C4 Button Interrupt/button_interrupt.c
@@ -10,18 +10,20 @@
 #include <avr/interrupt.h>
 
 int main(void) {
-	GIMSK |= _BV (INT0); 	// int - Enable external interrupts int0
-	MCUCR = _BV (ISC01); 	// Set rising/Falling edge. See ATtiny13 data sheet table 9-2
-	sei(); 					// Set Enable Global Interrupt bit
+	GIMSK |= _BV (INT0); // int - Enable external interrupts int0
+	//MCUCR = _BV (ISC01); // Set falling edge. See ATtiny13 data sheet table 9-2
+	MCUCR = _BV (ISC00); // Trigger on both rising/Falling edge. See ATtiny13 data sheet table 9-2
+	
+	sei(); // Set Enable Global Interrupt bit
 	DDRB = (1<<DDB4);
 	PORTB = 0;
-	for (;;){} 				//Loop forever. The interrupt will be triggered when the button is pressed.
+	for (;;){} //Loop forever. The interrupt will be triggered when the button is pressed.
 	return 0;
 }
 
 ISR (INT0_vect) // Interrupt Service Routine, performed when the interrupt is triggered
 {
-	PORTB ^= 1<<PB4;  //Toggle LED ON/OFF
+	PORTB ^= 1<<PB4; //Toggle LED ON/OFF
 	_delay_ms(500);
 	PORTB ^= 1<<PB4;
 	_delay_ms(500);
@@ -29,5 +31,4 @@ ISR (INT0_vect) // Interrupt Service Routine, performed when the interrupt is tr
 	_delay_ms(500);
 	PORTB ^= 1<<PB4;
 	_delay_ms(500);
-
 }
